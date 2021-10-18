1dc4c0d0281c7ba74786a9ee0c7a5a23b2cae08a
Nikita Isaev <keller108@yandex.ru>
2021-10-18 21:01:38 +0300

    JSX Main - st.4

--- nike-shop/src/components/App/App.js
+++ nike-shop/src/components/App/App.js
@@ -1,15 +1,13 @@
 import './App.scss';
 import Header from '../Header/Header';
-import mainBanner from '../../images/main-banner.jpg';
+import Main from '../Main/Main';
 
 function App() {
   return (
     <div className="app">
       <div class="project-container">
         <Header />
-        <div className="main-slider">
-          <img src={mainBanner} alt="Banner"/>
-        </div>
+        <Main />
       </div>
     </div>
   );
---
+++ nike-shop/src/components/Main/Main.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './Main.scss';
+
+function Main () {
+    return (
+        <main className="main">
+            <div className="main__top">
+                <h1>
+                    Все кроссовки
+                </h1>
+                <input type="text" defaultValue="Поиск..."/>
+            </div>    
+            <ul>
+            
+            </ul>
+        </main>
+    )
+};
+
+export default Main;
