d257d253ea99a3fae4e12604aff967f4c124cb75
Nikita Isaev <keller108@yandex.ru>
2021-10-28 21:15:49 +0500

    JSX Drawer st.1

---
+++ frontend/src/components/Drawer/Drawer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Drawer() {
+    return (
+        <div className="drawer-overlay">
+            <div className="drawer">
+                <h2 className="drawer__title">
+                    Корзина
+                </h2>
+                <ul className="drawer__list">
+                    <li className="drawer__item">
+
+                    </li>
+                </ul>
+                <div className="drawer__amount-wrapper">
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Drawer
