ff8a03c0b860e2495162863c07fc1d9db5be9a90
Nikita Isaev <keller108@yandex.ru>
2021-10-30 17:19:02 +0500

    JSX Drawer - top wrapper content created

--- frontend/src/components/Drawer/Drawer.jsx
+++ frontend/src/components/Drawer/Drawer.jsx
@@ -8,16 +8,18 @@ function Drawer({isOpen, onCartClose}) {
         <div className={isOpen ? `drawer-overlay drawer-overlay_visible` : `drawer-overlay`}>
             <div className="drawer">
                 <button onClick={onCartClose} className="drawer__close-btn" aria-label="close cart" type="button"/>
-                <h2 className="drawer__title">
-                    Корзина
-                </h2>
-                <ul className="drawer__list">
-                    <CardItem
-                        itemImage={itemImg1}
-                        itemTitle='Мужские Кроссовки Nike Blazer Mid Suede'
-                        itemPrice='12 999'
-                    />
-                </ul>
+                <div className="drawer__top-content-wrapper">
+                    <h2 className="drawer__title">
+                        Корзина
+                    </h2>
+                    <ul className="drawer__list">
+                        <CardItem
+                            itemImage={itemImg1}
+                            itemTitle='Мужские Кроссовки Nike Blazer Mid Suede'
+                            itemPrice='12 999'
+                        />
+                    </ul>
+                </div>
                 <div className="drawer__amount-wrapper">
                     <div className="drawer__amount-content">
                         <p className="drawer__amount-title">
